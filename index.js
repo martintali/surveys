6eb8e5334755029112200d0e259adc2e0b9bdb0b
Martin Daniel <martindaniel555@gmail.com>
2020-04-27 11:01:55 +0300

    Recieve the access token from google

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { googleClientID, googleClientSecret } = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there2' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
